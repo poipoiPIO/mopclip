cbd1b8425977754c3f79ee13c4e5b92042b3e562
Lily Appely <lappee@yahoo.com>
2022-09-30 13:49:18 +0300

    build setup updated

--- make.sh
+++ make.sh
@@ -1,6 +1,11 @@
 if [ $1 == "--interactive" ]
 then
   rlwrap smlnj src/helpers.sml src/types.sml src/combinators.sml src/parsers.sml
+fi
+
+if [ $1 == "--mlb" ] 
+then
+  ./third-party/sml-buildscripts/smlrepl src/mparcomb.mlb
 else
   mlton -output ./o.out src/mparcomb.mlb 
 fi
